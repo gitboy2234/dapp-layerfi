788e2b72d41c49cfe65a46ab4c0eee2341628079
renzivan243 <107743082+renzivan243@users.noreply.github.com>
2023-11-12 22:27:31 +0800

    fix on the screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
+        screens: {
+            sm: "480px", // Adjust the breakpoint to target smaller screens like phones
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
         extend: {},
     },
     plugins: [],
